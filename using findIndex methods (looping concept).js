8e135b6ae7db98e0f1cf17ab065b6edbf305db1a
Sivaprasanth-989 <sivaprasanth989@gmail.com>
2026-05-07 22:38:06 +0530

    Add findIndex example with console log for 7
    Implement findIndex method to locate first occurrence of 7 in the array.

---
+++ using findIndex methods (looping concept).js
@@ -0,0 +1,9 @@
+let myArray = [5, 6, 9, 1, 32, 7, 7];
+let result = myArray.findIndex(function(ele) {
+     if (ele === 7){
+         console.log("Thala for a reason");
+         return false
+     }
+});
+console.log(result);
+//  thats how the flow of  execution and performs upon the array  by using findIndex function method freaking loop logic by the way !!
