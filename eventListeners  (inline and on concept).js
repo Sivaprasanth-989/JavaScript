7e0590b11f69f869937543558fa591abe0bb4997
Sivaprasanth-989 <sivaprasanth989@gmail.com>
2026-05-20 13:23:36 +0530

    Implement greeting button click event listener
    Add event listener for greeting button click.

---
+++ eventListeners  (inline and on concept).js
@@ -0,0 +1,11 @@
+// on event listener!
+let greetBtn = document.getElementById("greet");
+
+greetBtn.onclick = function() {
+    console.log("Welcome home sir!");
+}
+
+// inline addEventListener
+// function greeting() {
+//     console.log("Welcome sir!");
+// }
